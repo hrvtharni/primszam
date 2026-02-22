a5872c1097c45e9c4071272b02f7ea276454f7b2
hrvtharni <arnoldhorvath@gmail.com>
2026-02-22 15:06:56 +0100

    Add project files.

---
+++ primszam/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static bool Prim(int szam)
+    { 
+        if (szam < 2)
+            return false;
+
+        for (int i = 2; i < szam; i++)
+        {
+            if (szam % i == 0)
+                return false;
+        }
+        return true;
+    }
+    static void Main()
+    {
+        Console.WriteLine("Adj meg egy számot:");
+        int szam = Convert.ToInt32(Console.ReadLine());
+
+        if (Prim(szam))
+        {
+            Console.WriteLine("A megadott szám prím");
+        }
+        else
+        {
+            Console.WriteLine("A megadott szám nem prím");
+        }
+        
+        Console.ReadKey();
+    }
+}
